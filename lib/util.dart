f81db93abae36076270c07b4db4c5b2aee96aad5
MYOUSUFFS <mailyousuffsend@gmail.com>
2024-01-12 15:47:02 +0530

    notification

--- lib/util.dart
+++ lib/util.dart
@@ -1,4 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class MyNotification {
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+  final String body;
+  final String title;
+
+  MyNotification(
+    this.flutterLocalNotificationsPlugin,
+    this.body,
+    this.title,
+  );
+
+  Future notification() async {
+    var _androidNotification =
+        AndroidInitializationSettings('mipmap/ic_launcher');
+    var _init = InitializationSettings(android: _androidNotification);
+    await flutterLocalNotificationsPlugin.initialize(_init);
+    final _androidNotificationDetails = AndroidNotificationDetails(
+      "@ecfile",
+      "Ecfile",
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+    final _notification =
+        NotificationDetails(android: _androidNotificationDetails);
+    flutterLocalNotificationsPlugin.show(0, title, body, _notification);
+  }
+}
 
 ScaffoldFeatureController<MaterialBanner, MaterialBannerClosedReason>
     materialBanner(
